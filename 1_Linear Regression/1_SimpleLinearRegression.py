3269fb89d9cbcd09a7ba5f5041ba17850e39a619
LOO HUI KIE <shonenent1024@gmail.com>
2024-04-03 22:29:15 +0800

    change files name

---
+++ 1_Linear Regression/1_SimpleLinearRegression.py
@@ -0,0 +1,107 @@
+''' import necessary library '''
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+'''     Reading the data    '''
+df = pd.read_csv("FuelConsumptionCo2.csv")
+
+# Print the first few rows of the DataFrame
+# print(df.head(10))
+
+'''     Data Exploration   '''
+# Summarize the data
+print(df.describe())
+
+# Select some features to explore more
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+print(cdf.head(9))
+
+# Plot each of the features
+viz = cdf[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+viz.hist()
+plt.show()
+
+# Plot these features againt Emission to see how linear their relationship is:
+plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS, color="blue")
+plt.xlabel("FUELCONSUMPTION_COMB")
+plt.ylabel("CO2EMISSIONS")
+plt.show()
+
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS, color="blue")
+plt.xlabel("ENGINESIZE")
+plt.ylabel("CO2EMISSIONS")
+plt.show()
+
+plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS, color="blue")
+plt.xlabel("CYLINDERS")
+plt.ylabel("CO2EMISSIONS")
+plt.show()
+
+'''  Creating train and test dataset  '''
+# randomly select 80% of the data to train and 20% to test
+msk     = np.random.rand(len(df)) < 0.8
+train   = cdf[msk]
+test    = cdf[~msk]
+
+'''  Simple Regresiion Model  '''
+# Train data distribution
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS, color='blue')
+plt.xlabel("ENGINESIZE")
+plt.ylabel("CO2EMISSIONS")
+plt.show()
+
+'''  Modeling  '''
+# Using the sklearn package to model data
+from sklearn import linear_model
+regr    = linear_model.LinearRegression()
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit(train_x, train_y)
+# The coefficients
+print('Coefficients                 :', regr.coef_)
+print('Intercept                    :', regr.intercept_)
+
+# Plot output
+plt.scatter(train.ENGINESIZE ,train.CO2EMISSIONS, color='blue')
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
+plt.xlabel("ENGINESIZE")
+plt.ylabel("Emission")
+plt.show()
+
+'''  Evaluation  '''
+from sklearn.metrics import r2_score
+
+test_x  = np.asanyarray(test[['ENGINESIZE']])
+test_y  = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_ = regr.predict(test_x)
+
+print("Mean absolute error          : %.2f" % np.mean(np.absolute(test_y_ - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+print("R2-score                     : %.2f" % r2_score(test_y , test_y_) )
+
+
+
+'''  Train using FUELCONSUMPTION_COMB  '''
+train_x = np.asanyarray(train[['FUELCONSUMPTION_COMB']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit(train_x, train_y)
+# The coefficients
+print('Coefficients                 :', regr.coef_)
+print('Intercept                    :', regr.intercept_)
+
+# Plot output
+plt.scatter(train.FUELCONSUMPTION_COMB ,train.CO2EMISSIONS, color='blue')
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
+plt.xlabel("FUEL CONSUMPTION")
+plt.ylabel("Emission")
+plt.show()
+
+'''  Evaluation  '''
+test_x  = np.asanyarray(test[['FUELCONSUMPTION_COMB']])
+test_y  = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_ = regr.predict(test_x)
+
+print("Mean absolute error          : %.2f" % np.mean(np.absolute(test_y_ - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+print("R2-score                     : %.2f" % r2_score(test_y , test_y_) )
