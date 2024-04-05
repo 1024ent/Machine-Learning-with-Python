d55f3e4df7e4f2def341fabeacc29f89b6783878
LOO HUI KIE <shonenent1024@gmail.com>
2024-04-05 23:34:09 +0800

    Added Videos and Notes

--- 1_Linear Regression/4_NonLinearRegeression.py
+++ 1_Linear Regression/4_NonLinearRegeression.py
@@ -1,3 +1,9 @@
-'''  Import Libraries  '''
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+
+'''  Read the data  '''
+df = pd.read_csv("china_gdp.csv")
+# print(df.head(9))
+
+'''  Exploring the data  '''
