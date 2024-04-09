4abf445a7bc01a6a8c4a5610ad9a175e95a1ac92
LOO HUI KIE <shonenent1024@gmail.com>
2024-04-09 11:23:51 +0800

    Added CSV file for k-means

---
+++ 4_Clustering/0_DownloadFIle.py
@@ -0,0 +1,17 @@
+import requests
+
+def download_csv(url, file_name):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(file_name, 'w') as f:
+            f.write(response.text)
+        print(f"CSV file '{file_name}' downloaded successfully")
+    else:
+        print(f"Failed to download CSV file from '{url}'")
+
+urls_and_filenames = [
+    ('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%204/data/Cust_Segmentation.csv','Cust_Segmentation.csv')
+]
+
+for url, filename in urls_and_filenames:
+    download_csv(url, filename)
