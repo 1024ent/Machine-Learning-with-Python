f222dcc559824d98bf3eb99ce1ef0e46ab14eb05
LOO HUI KIE <shonenent1024@gmail.com>
2024-04-10 23:45:24 +0800

    addded Credit Card Fraud Detection

---
+++ 2_Classification/2_2_CreditCardFraudDetection.py
@@ -0,0 +1,134 @@
+'''  Import library  '''
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import normalize, StandardScaler
+from sklearn.utils.class_weight import compute_sample_weight
+from sklearn.metrics import roc_auc_score
+import time
+import warnings
+warnings.filterwarnings('ignore')
+
+'''  Data Analysis  '''
+raw_data = pd.read_csv('creditcard.csv')
+print("There are " + str(len(raw_data))+ " obsevations in the credit card fraud dataset.")
+print("There are " + str(len(raw_data.columns))+ " variables in the dataset.")
+# print(data.head(10))
+
+# Inflate the original one 10 times 
+n_replicas = 10
+
+# Inflicate original datasets
+big_raw_data = pd.DataFrame(np.repeat(raw_data.values, n_replicas, axis=0), columns=raw_data.columns)
+print("There are " + str(len(big_raw_data)) + " observations in the inflated credit card fraud dataset.")
+print("There are " + str(len(big_raw_data.columns)) + " variables in the dataset.")
+
+# Get the set of distinct classes
+labels = big_raw_data.Class.unique()
+
+#  Get the count for each class
+sizes = big_raw_data.Class.value_counts().values
+
+# Plot the class value counts
+fig, ax = plt.subplots()
+ax.pie(sizes, labels=labels, autopct='%1.3f%%')
+ax.set_title('Target Variable Value Counts')
+plt.show()
+
+# Plot histogram that shows the distribution of these amounts
+plt.hist(big_raw_data.Amount.values, 6, histtype='bar', facecolor='g')
+plt.show()
+print("Minimum amount value is ", np.min(big_raw_data.Amount.values))
+print("Maximum amount value is ", np.max(big_raw_data.Amount.values))
+print("90% of the transactions have an amount less or equal than ", np.percentile(raw_data.Amount.values, 90))
+
+''' Data Preprocessing '''
+# data preprocessing such as scaling/normalization is typically useful for 
+# linear models to accelerate the training convergence
+
+# standardize features by removing the mean and scaling to unit variance
+big_raw_data.iloc[:, 1:30] = StandardScaler().fit_transform(big_raw_data.iloc[:, 1:30])
+data_matrix = big_raw_data.values
+
+# X: feature matrix (for this analysis, we exclude the Time variable from the dataset)
+X = data_matrix[:, 1:30]
+
+# y: labels vector
+y = data_matrix[:, 30]
+
+# data normalization
+X = normalize(X, norm="l1")
+
+# print the shape of the features matrix and the labels vector
+print('X.shape=', X.shape, 'y.shape=', y.shape)
+
+''' Data Train, Test and Split  '''
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
+print("X_train=", X_train.shape, "X_test=", X_test.shape)
+print("y_train=", y_train.shape, "y_test=", y_test.shape)
+
+'''  Build a Decision Tree Classifier model with Scikit-Learn  '''
+# compute the sample weights to be used as input to the train routine so that 
+# it takes into account the class imbalance present in this dataset
+w_train = compute_sample_weight('balanced', y_train)
+
+# import the Decision Tree Classifier Model from scikit-learn
+from sklearn.tree import DecisionTreeClassifier
+
+# for reproducible output across multiple function calls, set random_state to a given integer value
+sklearn_dt = DecisionTreeClassifier(max_depth=4, random_state=35)
+
+# train a Decision Tree Classifier using scikit-learn
+t0 = time.time()
+sklearn_dt.fit(X_train, y_train, sample_weight=w_train)
+sklearn_time = time.time()-t0
+print("[Scikit-Learn] Training time (s):  {0:.5f}".format(sklearn_time))
+
+'''  Build a Decision Tree Classifier model with Snap ML  '''
+# if not already computed, 
+# compute the sample weights to be used as input to the train routine so that 
+# it takes into account the class imbalance present in this dataset
+# w_train = compute_sample_weight('balanced', y_train)
+
+# import the Decision Tree Classifier Model from Snap ML
+from snapml import DecisionTreeClassifier
+
+# Snap ML offers multi-threaded CPU/GPU training of decision trees, unlike scikit-learn
+# to use the GPU, set the use_gpu parameter to True
+# snapml_dt = DecisionTreeClassifier(max_depth=4, random_state=45, use_gpu=True)
+
+# to set the number of CPU threads used at training time, set the n_jobs parameter
+# for reproducible output across multiple function calls, set random_state to a given integer value
+snapml_dt = DecisionTreeClassifier(max_depth=4, random_state=45, n_jobs=4)
+
+# train a Decision Tree Classifier model using Snap ML
+t0 = time.time()
+snapml_dt.fit(X_train, y_train, sample_weight=w_train)
+snapml_time = time.time()-t0
+print("[Snap ML] Training time (s):  {0:.5f}".format(snapml_time))
+
+
+'''  Evaluate the Scikit-Learn and Snap ML Decision Tree Classifier Models  '''
+# Snap ML vs Scikit-Learn training speedup
+training_speedup = sklearn_time/snapml_time
+print("[Decision Tree Classifier] Snap ML vs Scikit-Learn speedup: {0:0.2f}x".format(training_speedup))
+
+# run inference and compute the probabilities of the test samples 
+# to belong to the class of fraudulent transactions
+sklearn_pred = sklearn_dt.predict_proba(X_test)[:,1]
+
+# evaluate the Compute Area Under the Receiver Operating Characteristic 
+# Curve (ROC-AUC) score from the predictions
+sklearn_roc = roc_auc_score(y_test, sklearn_pred)
+print("[Scikit-Learn] ROC-AUC score : {0:0.3f}".format(sklearn_roc))
+
+# run inference and compute the probabilities of the test samples
+# to belong to the class of fraudulent transactions
+snapml_pred = snapml_dt.predict_proba(X_test)[:,1]
+
+# evaluate the Compute Area Under the Receiver Operating Characteristic
+# Curve (ROC-AUC) score from the prediction scores
+snapml_roc = roc_auc_score(y_test, snapml_pred)
+print("[Snamp ML] ROC_AUC score: {0:0.3f}".format(snapml_roc))
