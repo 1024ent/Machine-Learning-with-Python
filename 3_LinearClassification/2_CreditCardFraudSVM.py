a2a3c0fc194fccfe7b379ab0735603ece7c7a945
LOO HUI KIE <shonenent1024@gmail.com>
2024-04-12 11:28:31 +0800

    Added SVM Credit Card Fraud Prediction

---
+++ 3_LinearClassification/2_CreditCardFraudSVM.py
@@ -0,0 +1,149 @@
+'''  Import library  '''
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import normalize, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.utils.class_weight import compute_sample_weight
+from sklearn.metrics import hinge_loss, roc_auc_score
+import time
+import warnings
+warnings.filterwarnings('ignore')
+
+'''  Data Analysis  '''
+raw_data = pd.read_csv('creditcard.csv')
+print("There are " + str(len(raw_data)) + " observations in the datasets.")
+print("There are " + str(len(raw_data.columns)) + " varianles in the datasets.")
+print(raw_data.head(10))
+
+# Inflate the original one 10 times 
+n_replicas = 10
+
+# Inflicate original datasets
+big_raw_data = pd.DataFrame(np.repeat(raw_data.values, n_replicas, axis=0), columns=raw_data.columns)
+print("There are " + str(len(big_raw_data)) + " observations in the datasets.")
+print("There are " + str(len(big_raw_data.columns)) + " variables in the datasets")
+
+# Get the set of distinct classes
+labels = big_raw_data.Class.unique()
+
+# Get the count for each class
+sizes = big_raw_data.Class.value_counts().values
+
+# Plot the class value counts
+fig, ax = plt.subplots()
+ax.pie(sizes, labels=labels, autopct='%1.3f%%')
+ax.set_title('Target Variable Value Counts')
+plt.show()
+
+# Plot histogram that shows the distribution of these amounts
+plt.hist(big_raw_data.Amount.values, 6, histtype='bar', facecolor='g')
+plt.show()
+print("Minimum amount value is ", np.min(big_raw_data.values))
+print("Maximum amount value is ", np.max(big_raw_data.values))
+print("90% of the transactions have an amount less or equal than ",np.percentile(big_raw_data.Amount.values, 90))
+
+''' Data Preprocessing '''
+# data preprocessing such as scaling/normalization is typically useful for 
+# linear models to accelerate the training convergence
+
+# standardize features by removing the mean and scaling to unit variance
+big_raw_data.iloc[:, 1:30] = StandardScaler().fit_transform(big_raw_data.iloc[:, 1:30])
+data_matrix = big_raw_data.values
+
+# X: feature matrix (for this analysis, we exclude the Time variable from the dataset)
+X = data_matrix[:, 1:30]
+
+# y: labels vector
+y = data_matrix[:, 30]
+
+# data normalization
+X = normalize(X, norm="l1")
+
+# print the shape of the features matrix and the labels vector
+print('X.shape=', X.shape, 'y.shape=', y.shape)
+
+''' Data Train, Test and Split  '''
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
+print("X_train=", X_train.shape, "X_test=", X_test.shape)
+print("y_train=", y_train.shape, "y_test=", y_test.shape)
+
+'''  Build a Decision Tree Classifier model with Scikit-Learn  '''
+# compute the sample weights to be used as input to the train routine so that 
+# it takes into account the class imbalance present in this dataset
+w_train = compute_sample_weight('balanced', y_train)
+
+'''  Build a Support Vector Machine model with Scikit-Learn  '''
+# import the linear Support Vector Machine (SVM) model from Scikit-Learn
+from sklearn.svm import LinearSVC
+
+# instatiate a scikit-learn SVM model
+# to indicate the class imbalance at fit time, set class_weight='balanced'
+# for reproducible output across multiple function calls, set random_state to a given integer value
+sklearn_svm = LinearSVC(class_weight='balanced', random_state=31, loss="hinge", fit_intercept=False)
+
+# train a linear Support Vector Machine model using Scikit-Learn
+t0 = time.time()
+sklearn_svm.fit(X_train, y_train)
+sklearn_time = time.time() - t0
+print("[Scikit-Learn] Training time (s):  {0:.2f}".format(sklearn_time))
+
+'''  Build a Support Vector Machine model with Snap ML  '''
+# import the Support Vector Machine model (SVM) from Snap ML
+from snapml import SupportVectorMachine
+
+# in contrast to scikit-learn's LinearSVC, Snap ML offers multi-threaded CPU/GPU training of SVMs
+# to use the GPU, set the use_gpu parameter to True
+# snapml_svm = SupportVectorMachine(class_weight='balanced', random_state=25, use_gpu=True, fit_intercept=False)
+
+# to set the number of threads used at training time, one needs to set the n_jobs parameter
+snapml_svm = SupportVectorMachine(class_weight='balanced', random_state=25, n_jobs=4, fit_intercept=False)
+# print(snapml_svm.get_params())
+
+# train an SVM model using Snap ML
+t0 = time.time()
+model = snapml_svm.fit(X_train, y_train)
+snapml_time = time.time() - t0
+print("[Snap ML] Training time (s):  {0:.2f}".format(snapml_time))
+
+'''  Evaluate the Scikit-Learn and Snap ML Support Vector Machine Models  '''
+# compute the Snap ML vs Scikit-Learn training speedup
+training_speedup = sklearn_time/snapml_time
+print('[Support Vector Machine] Snap ML vs. Scikit-Learn training speedup : {0:.2f}x '.format(training_speedup))
+
+# run inference using the Scikit-Learn model
+# get the confidence scores for the test samples
+sklearn_pred = sklearn_svm.decision_function(X_test)
+
+# evaluate accuracy on test set
+acc_sklearn  = roc_auc_score(y_test, sklearn_pred)
+print("[Scikit-Learn] ROC-AUC score:   {0:.3f}".format(acc_sklearn))
+
+# run inference using the Snap ML model
+# get the confidence scores for the test samples
+snapml_pred = snapml_svm.decision_function(X_test)
+
+# evaluate accuracy on test set
+acc_snapml  = roc_auc_score(y_test, snapml_pred)
+print("[Snap ML] ROC-AUC score:   {0:.3f}".format(acc_snapml))
+
+''' Practice  '''
+# Compute the hinge loss metric for both sets of predictions. Print the hinge losses of Scikit-Learn and Snap ML.
+# get the confidence scores for the test samples
+sklearn_pred = sklearn_svm.decision_function(X_test)
+snapml_pred  = snapml_svm.decision_function(X_test)
+
+# import the hinge_loss metric from scikit-learn
+from sklearn.metrics import hinge_loss
+
+# evaluate the hinge loss from the predictions
+loss_snapml = hinge_loss(y_test, snapml_pred)
+print("[Snap ML] Hinge loss:   {0:.3f}".format(loss_snapml))
+
+# evaluate the hinge loss metric from the predictions
+loss_sklearn = hinge_loss(y_test, sklearn_pred)
+print("[Scikit-Learn] Hinge loss:   {0:.3f}".format(loss_snapml))
+
+# the two models should give the same Hinge loss
+
